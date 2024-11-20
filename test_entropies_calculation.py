7b8a0d8b82d0c6fec92542db2f19426e905e4acd
MatteoRobbiati <robbiati.matteo@gmail.com>
2024-11-20 15:46:15 +0100

    fix: adapt entropies test to modification of ansatze

--- test_entropies_calculation.py
+++ test_entropies_calculation.py
@@ -8,7 +8,7 @@ from tncdr.targets.entropies import stabilizer_renyi_entropy
 @click.command()
 @click.option('--nqubits', type=int, required=True, help='Number of qubits.')
 @click.option('--nlayers', type=int, required=True, help='Number of layers.')
-@click.option('--qibo_backend', type=str, required=True, help='Qibo backend to use.')
+@click.option('--qibo_backend', type=str, required=True, default="numpy", help='Qibo backend to use.')
 def main(nqubits, nlayers, qibo_backend):
     """
     Execute the circuit with the specified parameters and compute SRE.
@@ -29,14 +29,17 @@ def main(nqubits, nlayers, qibo_backend):
     sre = stabilizer_renyi_entropy(state=state, alpha=2)
     print(f"\nSRE for circuit with all zero as parameters (pure Clifford): {sre}")
 
-    # Sample a random state from the ansatz and compute SRE
-    random_state = ansatz.sample_random_state(random_seed=42)
-    sre = stabilizer_renyi_entropy(state=random_state, alpha=2)
+    # Fill circuit's parameters with random angles
+    ansatz.random_unitary(random_seed=42)
+    state = ansatz.circuit(nshots=1000).state()
+
+    sre = stabilizer_renyi_entropy(state=state, alpha=2)
     print(f"SRE for circuit with all random parameters (non-Clifford): {sre}")
 
-    # Sample a random quasi-Clifford state and compute SRE
-    random_state = ansatz.sample_random_quasi_clifford_state(cliff_fraction=0.7, random_seed=42)
-    sre = stabilizer_renyi_entropy(state=random_state, alpha=2)
+    # Fill the circuit with quasi-clifford angles and compute SRE
+    ansatz.random_quasi_clifford_unitary(cliff_fraction=0.7, random_seed=42)
+    state = ansatz.circuit(nshots=1000).state()
+    sre = stabilizer_renyi_entropy(state=state, alpha=2)
     print(f"SRE for circuit for a quasi-Clifford circuit: {sre}")
 
 if __name__ == '__main__':
