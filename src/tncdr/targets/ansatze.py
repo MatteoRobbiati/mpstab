e6983fb2799cdd3d8962b3a38ee260d533d08f8d
MatteoRobbiati <robbiati.matteo@gmail.com>
2025-01-10 12:02:28 +0100

    feat: working on the partitioning problem

--- src/tncdr/targets/ansatze.py
+++ src/tncdr/targets/ansatze.py
@@ -1,9 +1,14 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 from dataclasses import dataclass
+from copy import deepcopy
+import random 
 
 import numpy as np
 
 from qibo import Circuit, gates
+
+from dataclasses import dataclass
+from abc import ABC
 
 @dataclass
 class Ansatz(ABC):
@@ -11,17 +16,19 @@ class Ansatz(ABC):
     nqubits: int
     density_matrix: bool = False
 
-    def __post_init__(
-        self,
-    ):
+    def __post_init__(self):
         self._circuit = Circuit(nqubits=self.nqubits, density_matrix=self.density_matrix)
 
     @property
-    def circuit(
-        self,
-    ):
+    def circuit(self):
         return self._circuit
-    
+
+    @circuit.setter
+    def circuit(self, value):
+        if not isinstance(value, Circuit):
+            raise TypeError("Expected a Circuit instance")
+        self._circuit = value
+
     @property
     def nparams(self):
         return len(self.circuit.get_parameters())
@@ -52,6 +59,8 @@ class Ansatz(ABC):
     def execute(self, nshots: int = None):
         """Execute the circuit and return the outcome."""
         return self.circuit(nshots=nshots)
+        
+
 
 @dataclass
 class HardwareEfficient(Ansatz):
@@ -64,7 +73,66 @@ class HardwareEfficient(Ansatz):
             for q in range(self.nqubits):
                 self.circuit.add(gates.RY(q=q, theta=0.))
                 self.circuit.add(gates.RZ(q=q, theta=0.))
-            [ self.circuit.add(gates.CNOT(q0=q%self.nqubits, q1=(q+1)%self.nqubits)) for q in range(self.nqubits) ]
+            self.circuit += self.entanglement_layer()
         self.circuit.add(gates.M(*range(self.nqubits)))
+
+    
+    @property
+    def parameters_per_layers(self):
+        return int(self.nparams / self.nlayers)
     
 
+    def partitionate_circuit(self, n_partitions: int, magic_gates_per_partition: int = 1):
+        """
+        Construct a new circuit starting from the original so that it is in the form
+        T3 - C3 - T2 - C2 - T1 - C1 - ... etc, 
+        where C blocks are stabilizer circuits and T blocks are full of magic 🧙.
+        T blocks are composed of some of the rotations which preserve their original 
+        random angles, while C blocks are composed of the remaining rotations combined 
+        with the layer of CNOTS.
+        """
+        target_layers = [0]
+        target_layers.extend(list(random.sample(range(1, self.nlayers), n_partitions - 1)))
+        target_layers = np.sort(target_layers)
+        # Mapping layers into a dictionary
+        layers_map = {str(value): index for index, value in enumerate(target_layers)}
+        # These list will contain the circuits composing the partitionate circuit
+        # Each list will be containing a sub-circuit
+        magic_layers, stabilizer_layers = [Circuit(self.nqubits)] * n_partitions, [Circuit(self.nqubits)] * n_partitions
+
+        partitioned_circuit = Circuit(self.nqubits)
+
+        partition_block = 0
+        for i in range(self.nlayers):
+            if i in target_layers:
+                partition_block = layers_map[str(i)]
+                target_gates = list(random.sample(range(0, self.parameters_per_layers - 1), magic_gates_per_partition))
+                layer_gates = self.parametric_layer(layer_index=i)
+
+                for j, gate in layer_gates:
+                    if j in target_gates:
+                        partitioned_circuit.add(gate)
+                        magic_layers[partition_block].add(gate)
+                    else:
+                        gate.parameters = np.random.randint(-2, 3) * np.pi / 2
+                        partitioned_circuit.add(gate)
+                        stabilizer_layers[partition_block].add(gate)
+                
+
+
+
+
+
+    def parametric_layer(self, layer_index: int):
+        """Return the gates composing a parametric layer."""
+        # Start and end index for the layer in the circuit
+        # Count as start the number of parametric gates per layer + the entanglement layer
+        start_index = layer_index * self.parameters_per_layers + layer_index * self.nqubits
+        end_index = start_index + self.parameters_per_layers
+        return deepcopy(self.circuit.queue[start_index:end_index])
+    
+    def entanglement_layer(self):
+        """Construct an entanglement layer compatible with the target quantum circuit."""
+        ent_circuit = Circuit(self.nqubits)
+        [ ent_circuit.add(gates.CNOT(q0=q%self.nqubits, q1=(q+1)%self.nqubits)) for q in range(self.nqubits) ]
+        return ent_circuit
