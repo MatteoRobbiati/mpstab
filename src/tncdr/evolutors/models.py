6bf90d59135322457539b406a2e402f803661263
MatteoRobbiati <robbiati.matteo@gmail.com>
2025-02-14 12:04:59 +0100

    feat: proceeding with the MPO contraction implementation

--- src/tncdr/evolutors/models.py
+++ src/tncdr/evolutors/models.py
@@ -7,7 +7,7 @@ from qibo import Circuit
 from tncdr.evolutors.tensor_network import TensorNetwork
 from tncdr.evolutors.tensor_network.w_utils import (
     _compute_all_w_tensors,
-    # pauli_pauli_expansion,
+    pauli_pauli_expansion,
     basis_pauli_expansion,
     X_pauli_expansion,
     theta_pauli_expansion
@@ -39,6 +39,14 @@ class HybridSurrogate:
     @property
     def nqubits(self):
         return self.ansatz.circuit.nqubits
+    
+    @property
+    def n_rotation_layers(self):
+        """
+        Returns:
+            int: The number of rotation layers in the MPO.
+        """
+        return sum(1 for node in self.tn.tensornet.nodes if "Theta" in str(node))
 
     def sample_partition(self):
         """Sample a partition of the given ansatz."""
@@ -48,7 +56,7 @@ class HybridSurrogate:
         """Construct one horizontal layer of Ws, according to a given rotational gate."""
         
         # Add theta projection and X projection (very left and very right nodes of each row)
-        self.tn.add_tensor(f"Theta{layer_number}", tensor=theta_pauli_expansion(gate.parameters[0]))
+        self.tn.add_tensor(f"Theta{layer_number}", tensor=theta_pauli_expansion(theta=gate.parameters[0]))
         self.tn.add_tensor(id=f"X{layer_number}", tensor=X_pauli_expansion())
 
         # Add all the W operators
@@ -66,14 +74,14 @@ class HybridSurrogate:
                 self.tn.add_edge(f"T{q}", f"W0{q}", "v_link", (0,2))
             # Else, connect with the previous layer
             else:
-                self.tn.add_edge(f"W{layer_number - 1}{q}", f"W{layer_number}{q}", "v_link", (0,2))
+                self.tn.add_edge(f"W{layer_number - 1}{q}", f"W{layer_number}{q}", "v_link", (3,2))
 
         # Connect the first W of the row with the Theta node
         self.tn.add_edge(f"Theta{layer_number}", f"W{layer_number}0", 'h_link', (0,1))
 
         # Horizontal links
         for q in range(self.nqubits - 1):
-            self.tn.add_edge(f"W{layer_number}{q}", f"{layer_number}{q + 1}", "h_link", (0,1))
+            self.tn.add_edge(f"W{layer_number}{q}", f"W{layer_number}{q + 1}", "h_link", (0,1))
 
         # Link with X projection
         self.tn.add_edge(f"W{layer_number}{self.nqubits - 1}", f"X{layer_number}", "h_link", (0,0))
@@ -97,6 +105,48 @@ class HybridSurrogate:
         # construct the Network 
         for i, gate in enumerate(magic_circuit.queue):
             self.build_one_rotation_layer(gate, layer_number=i)
+
+
+    def contract_rotation_layer(self, layer_number: int):
+        """
+        Contract one rotation layer of the MPO. The upper directions are considered 
+        already contracted.
+        """
+        # Contract Theta with the first W
+        self.tn.contract(f"Theta{layer_number}", f"W{layer_number}0", "h_link", "")
+        # Contract the horizontal links among the Ws
+        for q in range(self.nqubits - 1):
+            self.tn.contract(f"W{layer_number}{q}", f"W{layer_number}{q + 1}", "h_link", (0,1))
+        # Contract the last W with X
+        self.tn.contract(f"W{layer_number}{self.nqubits - 1}", f"X{layer_number}", "h_link", (0,0))
+        # Contract the 
+
+    def contract_mpo_on_obs(self, observable: str):
+        """
+        Append the observable to the MPO and compute the contraction of 
+        the whole structure to get the expectation value. 
+        
+        Args:
+            observable (str): expected to be a string of Paulis.
+        """
+
+        # Connect to the observable
+        for n, pauli in enumerate(observable):
+            self.tn.add_tensor(f'O{n}', tensor=pauli_pauli_expansion(pauli))
+            self.tn.add_edge(f'O{n}', f'W{self.n_rotation_layers - 1}{n}', 'v_link', (0,3))
+
+        # Contract the O on the last layer of Ws
+        for n in range(len(observable)):
+            self.tn.contract(f"O{n}", f"W{self.n_rotation_layers - 1}{n}", "v_link", (0,3))
+        
+        # Now loop over the rotation layers and contract them all (in the Land of Mordor, where the Shadows lie)
+        for l in reversed(range(self.n_rotation_layers)):
+
+
+        import matplotlib.pyplot as plt
+        self.tn.draw()
+        plt.show()
+
 
 
 
