bf4b01cd53282bc6d88206b1c4b874e639c9b4bc
MatteoRobbiati <robbiati.matteo@gmail.com>
2025-02-14 16:22:04 +0100

    test: add test of the hybrid surrogate

---
+++ tests/test_hybrid_evolutor.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+from qibo import hamiltonians
+from qibo.symbols import X, Y, Z
+from tncdr.evolutors.models import HybridSurrogate
+from tncdr.targets.ansatze import HardwareEfficient
+
+
+# Construct the ansatz
+ansatz = HardwareEfficient(nqubits=3, nlayers=1)
+
+ansatz.circuit.set_parameters(np.random.randn(len(ansatz.circuit.get_parameters())))
+ansatz.circuit.draw()
+
+# Partitioning the circuit into magic and stabilizer blocks
+part_circ, mag_layers, stab_layers = ansatz.partitionate_circuit(
+    n_partitions=1,
+    magic_gates_per_partition=2,
+)
+
+evo = HybridSurrogate(ansatz)
+
+print("---------")
+mag_layers[0].draw()
+
+form = Z(0) * Z(1) * Z(2)
+ham = hamiltonians.SymbolicHamiltonian(form=form)
+print("EXPVAL CIRC:", ham.expectation(mag_layers[0]().state()))
+
+evo.mpo_from_magic_circuit(mag_layers[0])
+evo.contract_mpo_on_obs("ZZZ")
