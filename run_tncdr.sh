269c35a099336d9985962fe363682eb9be1d93ca
MatteoRobbiati <robbiati.matteo@gmail.com>
2025-03-19 10:26:32 +0100

    feat: bashscripts to run the code

---
+++ run_tncdr.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+python mitigation_run.py \
+  --nqubits 6 \
+  --nlayers 4 \
+  --nshots 2000 \
+  --ncircuits 20 \
+  --ansatz HardwareEfficient \
+  --observable ZZZZZZ \
+  --readout_bitflip_probability 0.005 \
+  --local_pauli_noise_sigma 0.005 \
+  --mitigation_method TNCDR \
+  --mitigation_args '{"npartitions": 3, "magic_gates_per_partition": 1, "max_bond_dimension": null}' \
+  --random_seed 42 \
+  --nruns 10
+
